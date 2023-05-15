aabe97a2ae761b94ea17893759e46b76dcaddaf9
Sjoerd Kanon <sjoerd@kanon.email>
2023-05-15 08:08:25 +0200

    Start Writing a new script to set baseline

--- Startup/Start-up-powershell.ps1
+++ Startup/Start-up-powershell.ps1
@@ -1,3 +1,5 @@
 #This script is the first in line to start te powershell of Company Name#
-.\baseline-functies.ps1
+."./baseline-functies.ps1"
 #The other scripts that is running in here comes one at the time at the place here#
+#Get the modules
+##line of script to install the modules
