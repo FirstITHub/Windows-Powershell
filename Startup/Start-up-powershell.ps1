fb895b3e5743c8c04ae0dfae398769891cf8b6f8
Sjoerd Kanon <sjoerd@kanon.email>
2023-05-15 08:17:10 +0200

    Added the greetings of the user

--- Startup/Start-up-powershell.ps1
+++ Startup/Start-up-powershell.ps1
@@ -1,3 +1,7 @@
+#Greetings of the user
+$userloggedin = (Get-ItemProperty "HKCU:\\Software\\Microsoft\\Office\\Common\\UserInfo\\").UserName
+Write-Host "Heey $userloggedin. Success vandaag!"
+
 #This script is the first in line to start te powershell of Company Name#
 ."./baseline-functies.ps1"
 #The other scripts that is running in here comes one at the time at the place here#
