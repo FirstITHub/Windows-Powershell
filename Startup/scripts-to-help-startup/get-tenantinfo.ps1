1db656d76f552f6493739926a64a9de72de3d64f
Sjoerd Kanon <sjoerd@kanon.email>
2023-05-17 11:52:15 +0000

    May 17, 2023, 1:52 PM

--- Startup/scripts-to-help-startup/get-tenantinfo.ps1
+++ Startup/scripts-to-help-startup/get-tenantinfo.ps1
@@ -1,6 +1,9 @@
 function f-gettenant
      {
-      $Customers = @(Get-MsolPartnerContract -DomainName $domainname)
+      $domainname = $domain
+      $global:connectmsoldomain = $domainname
+    $Customers = @()
+    $Customers = @(Get-MsolPartnerContract -TenantId $cid)
     Write-Host "You are logged in on"
     Write-Host "Tenant: $($Customers.name)"
     Write-Host "TenantID: $cid"
