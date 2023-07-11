1a57ab76d2102b0cee58c6789d9b2a3048219c7a
Sjoerd Kanon <sjoerd@kanon.email>
2023-07-11 07:59:56 +0200

    added lines to script

--- Exchange/Set-ooo-allusers.ps1
+++ Exchange/Set-ooo-allusers.ps1
@@ -3,15 +3,21 @@ $Mailboxes = Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails UserMailbox
 $Mailboxes | Select-Object DisplayName, UserPrincipalName, RecipientTypeDetails | Export-Csv -Path "C:\temp\File.csv" -NoTypeInformation
 
 
-# Specify the out-of-office message and start/end dates
+# Specify the out-of-office message and start/end dates, Remove ---- message -------
 $oomMessage = @"
 <html>
 <body>
-<p>Beste,</p>
+---header line----
+<p></p>
+---end of header line----
 <br>
-<p>Het volledige Arcade team geniet van een deugddoend bouwverlof. Onze kantoren zijn volledig gesloten vanaf maandag 10 juli tot en met vrijdag 28 juli. Op maandag 31 juli organiseren wij onze jaarlijkse “Arcade-dag”. Dinsdag 1 augustus staan wij opnieuw klaar om uw vragen te beantwoorden.</p>
+---body------
+<p></p>
 <br>
+----end body -----
+----signature-----
 <p>Zonnige groeten,</p>
+---- end signature -----
 </body>
 </html>
 "@
@@ -22,6 +28,7 @@ $endDate = Get-Date "2023-07-31 19:00:00"
 foreach ($mailbox in $Mailboxes){
     $identity = $mailbox.UserPrincipalName
     Set-MailboxAutoReplyConfiguration -Identity $identity -AutoReplyState Scheduled -StartTime $startDate -EndTime $endDate -ExternalMessage $oomMessage
+    Write-Host "Out-of-office message has been set for $identity in arcade-eng.com domain."
 }
 
 Write-Host "Out-of-office message has been set for all users in arcade-eng.com domain."
