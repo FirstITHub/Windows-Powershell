09138cad816faf353ecdc389dcf80fd9e7188414
Sjoerd Kanon <sjoerd@kanon.email>
2023-07-14 05:43:51 +0000

    Jul 14, 2023, 7:43 AM

---
+++ Partner-Connect/msgraph/Graph-Export-Logins.ps1
@@ -0,0 +1,69 @@
+# Install modules if they are not already present
+$modules = Get-Module
+
+$modulesToInstall = @(
+    "MSGraph",
+    "Microsoft.Graph"
+)
+
+$modulesToInstall | ForEach-Object {
+    $moduleName = $_
+    if ($moduleName -notin $modules) {
+# Install the required module (if not already installed)
+        Install-Module -Name $moduleName -AcceptLicense:$false -Confirm:$false
+        import-module -name $modulename
+    }
+    else {
+    }
+
+}
+# Authenticate and connect to Microsoft Graph
+
+Connect-MgGraph -TenantId 9d1b1b22-a9e0-4852-a110-fec4fd757c6e -Scopes "AuditLog.Read.All"
+
+
+
+
+# Set the start and end dates for the report (e.g., current month)
+
+$startDate = (Get-Date).AddMonths(-1).ToString("yyyy-MM-dd")
+
+$endDate = (Get-Date).ToString("yyyy-MM-dd")
+
+
+
+
+# Retrieve sign-in logs for the specified date range
+
+$signIns = Get-MgAuditLogSignIn -Filter "createdDateTime ge $startDate and createdDateTime le $endDate" |  Select-Object -Property UserDisplayName, UserPrincipalName, CreatedDateTime, @{Name="Country"; Expression={$_.Location.CountryOrRegion}}, @{Name="City"; Expression={$_.Location.City}}
+
+$count = $signins.count
+
+Write-Host = "There are $count"
+
+# Filter for logins abroad and select relevant properties
+
+$signInsAbroad = $signIns | Where-Object -Property Country -ne 'BE'
+
+
+
+
+# Export the sign-in logs to a CSV file
+
+$exportPath = "$env:OnedriveCommercial\Documenten\TEST\test.csv"
+
+$signInsAbroad | Export-Csv -Path $exportPath -NoTypeInformation
+
+
+
+
+# Confirmation message
+
+Write-Host "The sign-in logs for logins abroad in the last month have been exported to $exportPath."
+
+
+
+
+# Disconnect from Microsoft Graph
+
+Disconnect-MgGraph
